26d5fc821485a5b4df3fc11bb6ab617b7565f4a9
Elias Khattar <khattarelias@gmail.com>
2018-03-18 23:09:18 +0100

    Added button for input

--- components/input.js
+++ components/input.js
@@ -8,18 +8,43 @@ class Input extends Component {
     this.emit = emit
     this.state = state
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.submitButton = this.submitButton.bind(this)
   }
 
   createElement () {
     return html`
+    <div>
       <input
+         id="user-input"
          class="f3 f2-l helvetica bt-0 bl-0 br-0 bb-0 pb2 w-100"
          style="border-radius: 10px; padding: 5px;"
          placeholder="ask me something"
          autofocus
          onkeyup=${this.handleSubmit}
       />
+      <button onclick=${this.submitButton}>Send</button>
+    </div>
     `
+  }
+
+  submitButton () {
+    var input_text = document.getElementById("user-input").value
+    if (input_text !== ""){
+      this.emit('messages:add', {
+        text: input_text,
+        response: false
+      })
+      fetch(`http://${process.env.HOSTNAME}:3030/api/request?input=${input_text}&&id=${this.state.id}`)
+          .then(res => res.json())
+          .then(body => {
+            this.state.id = body.id
+            this.emit('messages:add', {
+              text: body.text,
+              response: true
+            })
+          })
+      console.log(input_text)
+    }
   }
 
   handleSubmit (evt) {
